de71b1bcc7296aff0a9ab078c574558f40e8c817
Claude Vedovini <592099+cvedovini@users.noreply.github.com>
2015-10-11 05:43:42 +0000

    cleaning up repo

--- class-admin.php
+++ class-admin.php
@@ -2,24 +2,18 @@
 
 class WPLinkedInCoAdmin {
 
-	function __construct() {
+	function WPLinkedInCoAdmin() {
 		if (function_exists('wp_linkedin_connection')) {
-			add_action('admin_menu', array(&$this, 'admin_menu'), 30);
+			$this->add_settings_field('wp-linkedin-co_fields',
+					__('Company fields', 'wp-linkedin-co'), 'settings_field_fields');
 		}
-
-		add_filter('plugin_action_links_' . WP_LINKEDIN_CO_PLUGIN_BASENAME, array(&$this, 'add_settings_link'));
+		add_filter('plugin_action_links_wp-linkedin-co/wp-linkedin-co.php', array(&$this, 'add_settings_link'));
 		add_action('admin_notices', array(&$this, 'admin_notices'));
-		add_action('network_admin_notices', array(&$this, 'admin_notices'));
-	}
-
-	function admin_menu() {
-		$this->add_settings_field('wp-linkedin-co_fields',
-				__('Company fields', 'wp-linkedin-co'), 'settings_field_fields');
 	}
 
 	function add_settings_link($links) {
-		$url = admin_url('options-general.php?page=wp-linkedin');
-		$links['settings'] = '<a href="' . $url . '">' . __('Settings') . '</a>';
+		$url = site_url('/wp-admin/options-general.php?page=wp-linkedin');
+		$links[] = '<a href="' . $url . '">' . __('Settings') . '</a>';
 		return $links;
 	}
 
@@ -31,21 +25,22 @@ class WPLinkedInCoAdmin {
 	function settings_field_fields() { ?>
 		<textarea id="wp-linkedin_fields" name="wp-linkedin-co_fields" rows="5"
 		cols="50"><?php echo get_option('wp-linkedin-co_fields', LINKEDIN_CO_FIELDS_DEFAULT); ?></textarea>
-		<p><em><?php _e('Comma separated list of fields to show on a company profile.', 'wp-linkedin-co'); ?>
-		<?php _e('You can overide this setting in the shortcode with the `fields` attribute.', 'wp-linkedin-co'); ?>
-		<?php printf(__('See the <a href="%s" target="_blank">LinkedIn API documentation</a> for the complete list of fields.', 'wp-linkedin-co'),
-				'https://developer.linkedin.com/docs/fields/company-profile'); ?></em></p><?php
+		<p><em><?php _e('Comma separated list of fields to show on a company profile.', 'wp-linkedin-co'); ?><br/>
+		<?php _e('You can overide this setting in the shortcode with the `fields` attribute.', 'wp-linkedin-co'); ?><br/>
+		<?php _e('See the <a href="https://developer.linkedin.com/documents/company-lookup-api-and-fields" target="_blank">LinkedIn API documentation</a> for the complete list of fields.', 'wp-linkedin-co'); ?></em></p><?php
 	}
 
 	function admin_notices() {
-		if (current_user_can('install_plugins')) {
-			if (!function_exists('wp_linkedin_connection')): ?>
-				<div class="error"><p><?php _e('The WP LinkedIn for Companies plugin needs the WP LinkedIn plugin to be installed and activated.', 'wp-linkedin-co'); ?></p></div>
-			<?php elseif (version_compare(WP_LINKEDIN_VERSION, '2.5') < 0):
-				$format = __('The WP LinkedIn for Company plugin requires at least version %s of the WP-LinkedIn plugin, current installed version is %s', 'wp-linkedin-co');
-				$error = sprintf($format, '2.5', WP_LINKEDIN_VERSION); ?>
-				<div class="error"><p><?php echo $error; ?></p></div>
-			<?php endif;
+		if (!function_exists('wp_linkedin_connection')) {
+			echo '<div class="error" style="font-weight:bold;"><ul><li>';
+			echo __('The WP LinkedIn for Companies plugin needs the WP LinkedIn plugin to be installed and activated.', 'wp-linkedin-co');
+			echo '</li></ul></div>';
+		} elseif (version_compare(WP_LINKEDIN_VERSION, '2.0') < 0) {
+			$format = __('The WP LinkedIn for Company plugin requires at least version %s of the WP-LinkedIn plugin, current installed version is %s', 'wp-linkedin-mu');
+			$error = sprintf($format, '2.0', WP_LINKEDIN_VERSION);
+			echo '<div class="error" style="font-weight:bold;"><ul><li>';
+			echo $error;
+			echo '</li></ul></div>';
 		}
 	}
 
